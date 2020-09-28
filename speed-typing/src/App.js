5e03fd071aa40ab305e856f5e822932f3814d54e
Ashim Ghimire <aghimire85@gmail.com>
2020-09-28 14:30:43 -0500

    Created a custom hooks to separate the logic from the App

--- speed-typing/src/App.js
+++ speed-typing/src/App.js
@@ -1,50 +1,18 @@
 import React, { useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useTypeWord from './components/hook/useTypeWord';
 
 function App() {
-  const START_TIME = 5;
-  const textAreaRef = useRef(null);
-
-  const [letter, setLetter] = useState('');
-  const [timer, setTimer] = useState(START_TIME);
-  const [isTimeRunning, setIsTimeRunning] = useState(false);
-  const [wordCount, setWordCount] = useState(0);
-
-  useEffect(() => {
-    if (isTimeRunning && timer > 0) {
-      setTimeout(() => {
-        setTimer((time) => time - 1);
-      }, 1000);
-    } else if (timer === 0) {
-      endTimer();
-    }
-  }, [timer, isTimeRunning]);
-
-  const handleChange = (e) => {
-    const { value } = e.target;
-    setLetter(value);
-  };
-
-  const countWord = (text) => {
-    const textArr = text.trim().split(' ');
-    const filteredArr = textArr.filter((word) => word !== '');
-    console.log(filteredArr.length);
-    return filteredArr.length;
-  };
-
-  const startTimer = () => {
-    setIsTimeRunning(true);
-    setLetter('');
-    setTimer(START_TIME);
-    setWordCount(0);
-    textAreaRef.current.disabled = false;
-    textAreaRef.current.focus();
-  };
-  const endTimer = () => {
-    setIsTimeRunning(false);
-    setWordCount(countWord(letter));
-  };
+  const {
+    textAreaRef,
+    letter,
+    handleChange,
+    isTimeRunning,
+    timer,
+    wordCount,
+    startTimer,
+  } = useTypeWord(10);
 
   return (
     <>
